0689ccfce0f4e967b35488cc9eefa10e0951f597
Joosung Ahn <joosung21@gmail.com>
2018-04-30 10:07:38 +0900

    Done BP Select

--- public/js/bp.js
+++ public/js/bp.js
@@ -15,7 +15,30 @@ $( document ).ready(function() {
 
   // DEMO ONLY: Calendar Click Event Listner
   $('.table-calendar .room').click(function(){
-    location.href='/bp-calendar-select'
+    var avail = $(this).children($('.status-badge')).hasClass('available');
+    if(avail === true){
+      location.href='/bp-calendar-select';
+    }
+  });
+
+  // 캘린더 전화문의 Hover
+  $('.table-calendar .room').mouseover(function(){
+    var callonly = $(this).children($('.status-badge')).hasClass('callonly');
+    if(callonly === true){
+      $(this).children($('.call-info')).addClass('show');
+    }
+    $(this).mouseout(function(){
+      $('.call-info').removeClass('show');
+    });
+  });
+
+  // PC테이블 전화문의 Hover
+  $('.btn-callonly').mouseover(function(){
+    $(this).parent().children($('.call-info')).addClass('show');
+
+    $(this).mouseout(function(){
+      $('.call-info2').removeClass('show');
+    });
   });
 
   // 모바일 달력 선택 Toggle
@@ -26,15 +49,6 @@ $( document ).ready(function() {
       $('#bookSelectedBtn').show();
     });
   });
-
-  // Carousel Swipe on Mobile
-  // $(".carousel").swiperight(function() {
-  //   $(this).carousel('prev');
-  // });
-  // $(".carousel").swipeleft(function() {
-  //   $(this).carousel('next');
-  // });
-
 
 });
 
