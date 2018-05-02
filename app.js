4756ab2d224fc6077255ddd8735a2b5a8444c7fc
Joosung Ahn <joosung21@gmail.com>
2018-05-02 16:26:10 +0900

    Done IE8 Interface

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use('/node_modules/', express.static('node_modules'))
 
 app.get('/', function (req, res){res.render('bp-pc')})
 app.get('/bp-calendar', function (req, res){res.render('bp-calendar')})
+app.get('/bp-calendar-ie8', function (req, res){res.render('bp-calendar-ie8')})
 app.get('/bp-calendar-select', function (req, res){res.render('bp-calendar-select')})
 app.get('/bp-book', function (req, res){res.render('bp-book')})
 app.get('/bp-find', function (req, res){res.render('bp-find')})
