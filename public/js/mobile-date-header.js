c5293123cc1ef79c58d0876b002200b266720621
Joosung Ahn <joosung21@gmail.com>
2018-08-08 16:41:50 +0900

    Edit mobile header

--- public/js/mobile-date-header.js
+++ public/js/mobile-date-header.js
@@ -4,13 +4,10 @@ var ticking = false;
 function doSomething(scroll_pos) {
   var windowWidth = $(window).width();
 
-  if(scroll_pos > 60) {
-    console.log('work');
-    $('.mobile-calendar-controler').css('top', (scroll_pos - 56) + 'px');
-    $('.mobile-calendar-controler').addClass('sticky');
+  if(scroll_pos > 199) {
+    $('.table-mobile-calendar-header').css('top', (scroll_pos - 199) + 'px');
   } else {
-    $('.mobile-calendar-controler').css('top', 0);
-    $('.mobile-calendar-controler').removeClass('sticky');
+    $('.table-mobile-calendar-header').css('top', 0);
   }
 
 }
